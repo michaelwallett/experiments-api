eca3efa58e33c1d1d38af71e94be376b6c6ce130
Michael Wallett <michael.wallett@gmail.com>
2014-09-06 23:44:40 +0100

    saving experiment to ES

--- index.js
+++ index.js
@@ -16,10 +16,27 @@ server.route({
           index: 'targeting',
           type: 'request',
           body: {
-            doc: _.extend(request.query, request.params)
+            doc: _.extend(request.params, request.query)
           }
         }).then(function (body) {
           reply(body);
+        }, function (error) {
+          console.trace(error.message);
+        });
+    }
+});
+
+server.route({
+    method: 'PUT',
+    path: '/experiments/{name}',
+    handler: function (request, reply) {
+        client.index({
+          index: 'experiments',
+          type: 'experiment',
+          id: request.params.name,
+          body: request.payload
+        }).then(function (body) {
+          reply();
         }, function (error) {
           console.trace(error.message);
         });
